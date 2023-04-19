fd8d5251a492e176b430774555c49943a1340c52
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-04-18 22:18:09 -0300

    up

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '',
+        pathname: '/**/**/**/**/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
