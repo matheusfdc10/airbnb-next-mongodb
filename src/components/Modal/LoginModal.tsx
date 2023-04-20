bcb30a44431a8f074b7cd60b623ab3487be9f5ff
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-04-20 01:05:35 -0300

    up

--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -96,13 +96,15 @@ const LoginModal = () => {
     const footerContent = (
         <div className="flex flex-col gap-4 mt-3">
             <hr />
-            <Button 
+            <Button
+                disabled
                 outline
                 label="Continue with Google"
                 icon={FcGoogle}
                 onClick={() => signIn('google')}
             />
-            <Button 
+            <Button
+                disabled
                 outline
                 label="Continue with GitHub"
                 icon={AiFillGithub}
